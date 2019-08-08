6ef3a25644368882edf133e22302e802f0bf4d3c
triggering-event <gabriel.j.shapiro@gmail.com>
2019-08-07 22:11:43 -0400

    final commit for project

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import {Home, About, Contact, Navigation} from './components';
 import {Route} from 'react-router-dom';
 
-<Route exact path="/"/ component={Home}/>;
-<Route path="/about"/ component={About}/>;
-<Route path="/contact"/ component={Contact}/>;
 
 
 const App = () => (
     <div>
         <Navigation/>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/contact" component={Contact}/>
     </div>
 );
 
