9538c468c48b05b8d574971b76e53605da02bb7f
triggering-event <gabriel.j.shapiro@gmail.com>
2019-08-07 21:48:39 -0400

    adds three routes for home, about and contact

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
-import { Home, About, Contact, Navigation } from './components';
-import { Route } from 'react-router-dom';
+import {Home, About, Contact, Navigation} from './components';
+import {Route} from 'react-router-dom';
+
+<Route path="/"/ component={Home}/>;
+<Route path="/about"/ component={About}/>;
+<Route path="/contact"/ component={Contact}/>;
+
 
 const App = () => (
-  <div>
-    <Navigation />
-  </div>
+    <div>
+        <Navigation/>
+    </div>
 );
 
 export default App;
