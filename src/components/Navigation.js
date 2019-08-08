fe7e700321a3896e4b79cce5ec65aa7aaac5b0dc
triggering-event <gabriel.j.shapiro@gmail.com>
2019-08-07 22:03:36 -0400

    declares the to as the href on links of navigation.js and specifies the correct routes for each sub-page

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 const Navigation = () => {
-  return (
-    <div>
-      <div className="App">
-        <h1>React Router Mini</h1>
+    return (
         <div>
-          <a href="">Home</a>
+            <div className="App">
+                <h1>React Router Mini</h1>
+                <div>
+                    <Link to="/">Home</Link>
+                </div>
+                <div>
+                    <Link to="/about">About</Link>
+                </div>
+                <div>
+                    <Link to="/Contact">Contact</Link>
+                </div>
+            </div>
         </div>
-        <div>
-          <a href="">About</a>
-        </div>
-        <div>
-          <a href="">Contact</a>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Navigation;
+
+
+
